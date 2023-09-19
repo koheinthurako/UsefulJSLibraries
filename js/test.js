13e9d8177bf1c8192e7132818f211322a47d8513
koheinthurako <captainthura99@gmail.com>
2023-09-19 21:57:48 +0630

    add moment datas

--- js/test.js
+++ js/test.js
@@ -1,3 +1,4 @@
+// Loadash
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // arr ကို က္ငင်းထဲက သတ်မှတ်ထားတဲ့ အရေအတွက် အလျောက် 3Dimensional Array များခွဲပေးခြင်း
@@ -6,3 +7,17 @@ console.log(_.chunk(arr, 3));
 const myName = "Thet Zaw Hein";
 
 console.log(_.camelCase(myName));
+
+
+// Moment
+const m = moment();
+m.add(45, "days");
+
+console.log(m.format("Y M D"));
+console.log(m.format("Y MM D"));
+console.log(m.format("Y MMM D"));
+
+m.subtract(3, "years");
+m.subtract(25, "days");
+
+console.log(m.format("Y MMM D"));
