0798c2145e35562c92cceaf9b82088b681735c47
koheinthurako <captainthura99@gmail.com>
2023-09-19 21:14:37 +0630

    add moment

--- js/test.js
+++ js/test.js
@@ -2,3 +2,7 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // arr ကို က္ငင်းထဲက သတ်မှတ်ထားတဲ့ အရေအတွက် အလျောက် 3Dimensional Array များခွဲပေးခြင်း
 console.log(_.chunk(arr, 3));
+
+const myName = "Thet Zaw Hein";
+
+console.log(_.camelCase(myName));
